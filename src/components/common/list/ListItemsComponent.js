286e765f483c5be18283327d0f9d9acdb5fd61d0
Pascal Nitcheu <nitcheupascal@gmail.com>
2022-01-09 18:10:31 +0100

    setting ListItemsComponent and passing ItemComponent in the render Item

--- src/components/common/list/ListItemsComponent.js
+++ src/components/common/list/ListItemsComponent.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import {FlatList,ScrollView,Text} from 'react-native';
+import ItemComponent from '../item/ItemComponent';
+
+const ListItemsComponent = (props) =>{
+    return (
+        <FlatList 
+            data={props.data?props.data:[]}
+            renderItem={ (elmnt) =>  <ItemComponent item={elmnt.item}/>}
+            keyExtractor={item=> item.id}
+
+        />
+    ) 
+}
+export default ListItemsComponent;
